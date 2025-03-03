b348dc17fec1afce61456ed081b73e6cd9c88a29
Nikita Shakirov <nikita.shakirov@thedott.io>
2025-03-03 13:29:25 +0900

    #8書籍の削除機能を追加しました

--- intern-book-app/src/app/components/book-card/book-card.component.ts
+++ intern-book-app/src/app/components/book-card/book-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Book} from '../../types/book';
 import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
 import {NgForOf} from '@angular/common';
@@ -19,4 +19,9 @@ import {bookList} from '../../consts/bookList';
 })
 export class BookCardComponent {
   @Input() books!: Book[];
+  @Output() deleteBook = new EventEmitter<Book>();
+
+  delete(book: Book) {
+    this.deleteBook.emit(book);
+  }
 }
