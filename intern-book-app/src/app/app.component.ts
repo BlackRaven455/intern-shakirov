e516891a1af382a01ef0863642b79e53fbd3a846
Nikita Shakirov <nikita.shakirov@thedott.io>
2025-03-06 00:11:32 +0900

    #11 操作ログを表示する機能を追加しました

--- intern-book-app/src/app/app.component.ts
+++ intern-book-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {ToolbarComponent} from './components/toolbar/toolbar.component';
+import {MessageComponent} from './components/message/message.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToolbarComponent],
+  imports: [RouterOutlet, ToolbarComponent, MessageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
