7365c3c69e7d4b19832ba0c7fc152b578d44dcc1
Nikita Shakirov <nikita.shakirov@thedott.io>
2025-03-13 13:51:47 +0900

    ＃18 検索機能を作成しました

--- intern-book-app/src/app/components/book-card/book-card.component.ts
+++ intern-book-app/src/app/components/book-card/book-card.component.ts
@@ -1,37 +1,58 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Book} from '../../types/book';
 import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
-import {NgForOf} from '@angular/common';
+import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
 import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 import {DialogConfirmComponent} from '../dialog-confirm/dialog-confirm.component';
-import {Observable} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {MessageService} from '../../services/message.service';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {FormControl, FormsModule} from '@angular/forms';
+import {MatInput} from '@angular/material/input';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
+import {toRomaji} from 'wanakana';
+import {MatOption, MatSelect} from '@angular/material/select';
 
 @Component({
   selector: 'app-book-card',
   imports: [
     MatCard,
+    MatLabel,
     MatCardActions,
     MatCardContent,
     MatCardHeader,
     MatCardTitle,
-    NgForOf
+    NgForOf,
+    MatFormField,
+    FormsModule,
+    AsyncPipe,
+    MatInput,
+    MatSelect,
+    MatOption,
   ],
   templateUrl: './book-card.component.html',
   styleUrl: './book-card.component.css'
 })
-export class BookCardComponent {
+export class BookCardComponent implements OnInit {
   @Input() books!: Book[];
   @Output() deleteBook = new EventEmitter<Book>();
+  filteredBooks = new BehaviorSubject<Book[]>(this.books);
+  searchTerm: string = `name`;
+
 
   constructor(public dialog: MatDialog, public messageService: MessageService) {
+  }
+
+  ngOnInit() {
+    this.filteredBooks.next(this.books); // Initialize filteredBooks with all books
   }
 
   delete(book: Book) {
     this.openDialog().subscribe(result => {
       if (result) {
         this.deleteBook.emit(book);
-        this.messageService.add(book.name, `Deleted`)
+        this.messageService.add(book.name, `Deleted`);
       }
     });
   }
@@ -39,5 +60,16 @@ export class BookCardComponent {
   openDialog(): Observable<boolean> {
     let dialogRef = this.dialog.open(DialogConfirmComponent);
     return dialogRef.afterClosed();
+  }
+
+  search(value: string) {
+    console.log(this.searchTerm);
+    if (this.searchTerm === `name`) {
+      const normalizedSearch = toRomaji(value.toLowerCase());
+      this.filteredBooks.next(this.books.filter((book: Book) => toRomaji(book.name.toLowerCase()).includes(normalizedSearch)));
+    } else {
+
+      this.filteredBooks.next(this.books.filter((book: Book) => book.evaluation.includes(value.toLowerCase())));
+    }
   }
 }
--- intern-book-app/src/app/components/message/message.component.ts
+++ intern-book-app/src/app/components/message/message.component.ts
@@ -2,28 +2,30 @@ import {Component} from '@angular/core';
 import {MessageService} from '../../services/message.service';
 import {NgForOf, NgIf} from '@angular/common';
 import {MatButton} from '@angular/material/button';
+import {MatSlideToggle} from "@angular/material/slide-toggle";
 
 @Component({
-  selector: 'app-message',
-  imports: [
-    NgForOf,
-    MatButton,
-    NgIf
-  ],
-  templateUrl: './message.component.html',
-  styleUrl: './message.component.css'
+    selector: 'app-message',
+    imports: [
+        NgForOf,
+        MatButton,
+        NgIf,
+        MatSlideToggle
+    ],
+    templateUrl: './message.component.html',
+    styleUrl: './message.component.css'
 })
 export class MessageComponent {
-  isVisible = false;
+    isVisible = false;
 
-  constructor(protected messageService: MessageService) {
-  }
+    constructor(protected messageService: MessageService) {
+    }
 
-  showContent() {
-    this.isVisible = !this.isVisible;
-  }
+    showContent() {
+        this.isVisible = !this.isVisible;
+    }
 
-  clear() {
-    this.messageService.clear();
-  }
+    clear() {
+        this.messageService.clear();
+    }
 }
