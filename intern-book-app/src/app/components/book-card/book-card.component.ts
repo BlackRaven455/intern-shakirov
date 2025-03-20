5b22745639200887be3017f5e00db7b7e97dc233
Nikita Shakirov <nikita.shakirov@thedott.io>
2025-03-20 17:55:50 +0900

    Revert "＃18 検索機能を作成しました"
    This reverts commit 7365c3c69e7d4b19832ba0c7fc152b578d44dcc1.

--- intern-book-app/src/app/components/book-card/book-card.component.ts
+++ intern-book-app/src/app/components/book-card/book-card.component.ts
@@ -1,58 +1,37 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Book} from '../../types/book';
 import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
-import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
+import {NgForOf} from '@angular/common';
 import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 import {DialogConfirmComponent} from '../dialog-confirm/dialog-confirm.component';
-import {BehaviorSubject, Observable} from 'rxjs';
+import {Observable} from 'rxjs';
 import {MessageService} from '../../services/message.service';
-import {MatFormField, MatLabel} from '@angular/material/form-field';
-import {FormControl, FormsModule} from '@angular/forms';
-import {MatInput} from '@angular/material/input';
-import {MatIcon} from '@angular/material/icon';
-import {MatIconButton} from '@angular/material/button';
-import {toRomaji} from 'wanakana';
-import {MatOption, MatSelect} from '@angular/material/select';
 
 @Component({
   selector: 'app-book-card',
   imports: [
     MatCard,
-    MatLabel,
     MatCardActions,
     MatCardContent,
     MatCardHeader,
     MatCardTitle,
-    NgForOf,
-    MatFormField,
-    FormsModule,
-    AsyncPipe,
-    MatInput,
-    MatSelect,
-    MatOption,
+    NgForOf
   ],
   templateUrl: './book-card.component.html',
   styleUrl: './book-card.component.css'
 })
-export class BookCardComponent implements OnInit {
+export class BookCardComponent {
   @Input() books!: Book[];
   @Output() deleteBook = new EventEmitter<Book>();
-  filteredBooks = new BehaviorSubject<Book[]>(this.books);
-  searchTerm: string = `name`;
-
 
   constructor(public dialog: MatDialog, public messageService: MessageService) {
-  }
-
-  ngOnInit() {
-    this.filteredBooks.next(this.books); // Initialize filteredBooks with all books
   }
 
   delete(book: Book) {
     this.openDialog().subscribe(result => {
       if (result) {
         this.deleteBook.emit(book);
-        this.messageService.add(book.name, `Deleted`);
+        this.messageService.add(book.name, `Deleted`)
       }
     });
   }
@@ -60,16 +39,5 @@ export class BookCardComponent implements OnInit {
   openDialog(): Observable<boolean> {
     let dialogRef = this.dialog.open(DialogConfirmComponent);
     return dialogRef.afterClosed();
-  }
-
-  search(value: string) {
-    console.log(this.searchTerm);
-    if (this.searchTerm === `name`) {
-      const normalizedSearch = toRomaji(value.toLowerCase());
-      this.filteredBooks.next(this.books.filter((book: Book) => toRomaji(book.name.toLowerCase()).includes(normalizedSearch)));
-    } else {
-
-      this.filteredBooks.next(this.books.filter((book: Book) => book.evaluation.includes(value.toLowerCase())));
-    }
   }
 }
